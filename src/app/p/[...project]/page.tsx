38a0d36c35eadc9b2473f7727ab906fd5674f373
Ethan Coward <ethan@ethancoward.dev>
2024-10-07 22:02:33 +0100

    Add placeholder for project specific page

---
+++ src/app/p/[...project]/page.tsx
@@ -0,0 +1,17 @@
+export default function ProjectPage() {
+  return (
+    <div className="mx-auto flex h-full max-w-2xl flex-col items-center px-4">
+      <div className="px-4 pt-8 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-3xl">
+          <h1 className="mb-4 text-2xl font-bold text-gray-900 sm:text-3xl dark:text-gray-100">
+            UH OH... The project pages are still in development!
+          </h1>
+          <p className="mb-4 text-gray-800 sm:text-lg dark:text-gray-300">
+            Come back later to see them!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export const runtime = "edge";
